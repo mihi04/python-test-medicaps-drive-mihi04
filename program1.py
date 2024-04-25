8f9a0a993fd69a249f8fb35b4b320627a9ef91be
Mihi_Pal <85250967+mihi04@users.noreply.github.com>
2024-04-25 14:18:38 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,13 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        pair = dict(('()', '[]', '{}'))
+        st = []
+        for x in s:
+            if x in '([{':
+                st.append(x)
+            elif len(st) == 0 or x != pair[st.pop()]:
+                return False
+        return len(st) == 0
     
 
 
